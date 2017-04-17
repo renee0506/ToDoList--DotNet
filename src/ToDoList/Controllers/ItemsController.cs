ef2562ef9b43ea902708495ddea2bbe69333836e
Nicole Sanders and Renee Mei <nicole+renee@>
2017-04-17 10:25:02 -0700

    set up the files and link to the database. Displays the item list and a detail page for each item

---
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: /<controller>/
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+    }
+
+}
---
+++ src/ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoList2;integrated security=True");
+        }
+    }
+}
